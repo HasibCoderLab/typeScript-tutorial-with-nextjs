5640abb6734ab0f26be0a9093011b30f785ccc96
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-11 21:08:36 +0600

    feat:create file

--- src/function.ts
+++ src/function.ts
@@ -1,5 +1,19 @@
-function tsf (a:number ,b:string) {
+// function tsf (a:number ,b:string) {
 
+// }
+
+// tsf(20,"20")
+
+// function tsf (a:number ,b:string):number {
+// return 15;
+// }
+
+// tsf(20,"20");
+
+
+
+// ==================== Optional & Default Parametar ============
+function tsf (a:number ,b:string="Hasib") {
 }
 
-tsf(20,"20")
+tsf(20);
