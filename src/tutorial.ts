b32a31217f9167c726b2f386f95ab1f2e1646e4f
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-11 19:41:41 +0600

    learn basic of typescript

---
+++ src/tutorial.ts
@@ -0,0 +1,26 @@
+//  =========     Basic of Typescript=========
+
+let age:number = 18;
+age=10+1;
+
+let user:string= "Hasib Hasan";
+user = "Fahim"
+
+let Marks:number[] = [14,18,41,14,19,23,16,15.81,0.2];
+// marstdMarksks = [85,14,55,78,26,25,544,];
+
+let names:string[] = ["Hasib" ,"Fahim", "Rifat", "Hasan"];
+names = ["Akash" , "Shamim" , "Mahbub"];
+
+
+let isOnline:boolean;
+
+// isOnline= "Hi"
+isOnline = true;
+
+
+
+// ======  Tuple ============
+
+let twoWord :[string,boolean] ;
+twoWord=["Hasib" , true]
